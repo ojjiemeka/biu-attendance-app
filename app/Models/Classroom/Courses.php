d4b94e8f852ef66d72187437318263d875f11866
Chukwuemeke Ojji <ojjiemeka@gmail.com>
2023-03-05 01:07:52 +0100

    models and migration updated

--- app/Models/Classroom/Courses.php
+++ app/Models/Classroom/Courses.php
@@ -2,10 +2,43 @@
 
 namespace App\Models\Classroom;
 
+use App\Models\Auth\Lecturer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Courses extends Model
 {
     use HasFactory;
+
+    protected $table = 'courses';
+
+    const LEVEL_100            = '100 LEVEL';
+    const LEVEL_200            = '200 LEVEL';
+    const LEVEL_300            = '300 LEVEL';
+    const LEVEL_400            = '400 LEVEL';
+
+    protected $fillable = [
+        'uuid',
+        'course_code',
+        'course_desc',
+        'lecturer_id',
+        'duration',
+    ];
+
+    public static function getLevels(): array
+    {
+        return [
+            self::LEVEL_100                => '100 LEVEL',
+            self::LEVEL_200                => '200 LEVEL',
+            self::LEVEL_300                => '300 LEVEL',
+            self::LEVEL_400                => '400 LEVEL',
+        ];
+    }
+
+    public function lecturers(): HasMany
+    {
+        return $this->hasMany(Lecturer::class);
+    }
 }
---
+++ database/migrations/2023_03_04_233855_setup_tables.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Models\Auth\Lecturer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('core_lecturers', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->foreignId('user_id');
+            $table->char('status', 12)->default(Lecturer::STATUS_ACTIVE);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('core_students', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->char('mat_number', 15);
+            $table->string('department');
+            $table->string('faculty');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('core_courses', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->string('course_code');
+            $table->string('course_desc');
+            $table->string('lecturer_id');
+            $table->string('duration');
+            $table->string('level');
+            $table->string('department');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+
+        Schema::create('core_attendance', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->foreignId('course_id');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->char('mat_number', 15);
+            $table->string('department');
+            $table->string('faculty');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('core_lecturers');
+        Schema::dropIfExists('core_students');
+        Schema::dropIfExists('core_courses');
+        Schema::dropIfExists('core_attendance');
+    }
+};
